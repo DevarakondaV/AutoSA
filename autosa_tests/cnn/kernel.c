00281797c44846b96570afb098dbe533c15351f1
devarakondav <devarakonda.vishnu5@gmail.com>
2021-11-29 23:11:54 -0800

    reset dims

--- autosa_tests/cnn/kernel.c
+++ autosa_tests/cnn/kernel.c
@@ -15,30 +15,30 @@ int main(int argc, char **argv){
 
   LoadData(input, weight, bias);
 
-  // data_t cin[R + K - 1][C + K - 1][I];
-  // data_t w[O][K][K][I];
-  // data_t cout[R][C][O];
-  // data_t cout_golden[R][C][O];
+  data_t cin[R + K - 1][C + K - 1][I];
+  data_t w[O][K][K][I];
+  data_t cout[R][C][O];
+  data_t cout_golden[R][C][O];
 
-  data_t cin[I][R + K - 1][C + K - 1];
-  data_t w[I][O][K][K];
-  data_t cout[O][R][C];
-  data_t cout_golden[O][R][C];
+  // data_t cin[I][R + K - 1][C + K - 1];
+  // data_t w[I][O][K][K];
+  // data_t cout[O][R][C];
+  // data_t cout_golden[O][R][C];
 
   // data initialization
   for (int i = 0 ; i < I; i++)
     for (int r = 0; r < R + K - 1; r++)
       for (int c = 0; c < C + K - 1; c++) {
-        // cin[r][c][i] = i;
-        cin[i][r][c] = i;
+        cin[r][c][i] = i;
+        // cin[i][r][c] = i;
       }
 
   for (int o = 0; o < O; o++)
     for (int i = 0; i < I; i++) 
       for (int p = 0; p < K; p++)
         for (int q = 0; q < K; q++) {
-          // w[o][p][q][i] = o;
-          w[i][o][p][q] = o;
+          w[o][p][q][i] = o;
+          // w[i][o][p][q] = o;
         }
  
 #pragma scop
@@ -49,8 +49,8 @@ int main(int argc, char **argv){
         for (int i = 0; i < I; i++)
           for (int p = 0; p < K; p++)
             for (int q = 0; q < K; q++) {
-              // cout[r][c][o] = cout[r][c][o] + cin[r + p][c + q][i] * w[o][p][q][i];
-              cout[o][r][c] = cout[o][r][c] + cin[i][r + p][c + q] * w[i][o][p][q];
+              cout[r][c][o] = cout[r][c][o] + cin[r + p][c + q][i] * w[o][p][q][i];
+              // cout[o][r][c] = cout[o][r][c] + cin[i][r + p][c + q] * w[i][o][p][q];
             }
       }
 #pragma endscop  
@@ -63,8 +63,8 @@ int main(int argc, char **argv){
         for (int i = 0; i < I; i++)
           for (int p = 0; p < K; p++)
             for (int q = 0; q < K; q++) {
-              // cout_golden[r][c][o] = cout_golden[r][c][o] + cin[r + p][c + q][i] * w[o][p][q][i];
-              cout_golden[o][r][c] = cout_golden[o][r][c] + cin[i][r + p][c + q] * w[i][o][p][q];
+              cout_golden[r][c][o] = cout_golden[r][c][o] + cin[r + p][c + q][i] * w[o][p][q][i];
+              // cout_golden[o][r][c] = cout_golden[o][r][c] + cin[i][r + p][c + q] * w[i][o][p][q];
             }
       }
 
@@ -73,8 +73,8 @@ int main(int argc, char **argv){
   for (int o = 0; o < O; o++)
     for (int r = 0; r < R; r++)
       for (int c = 0; c < C; c++) {
-        // if (fabs((float)cout_golden[r][c][o] - (float)cout[r][c][o]) > thres) {
-        if (fabs((float)cout_golden[o][r][c] - (float)cout[o][r][c]) > thres) {
+        if (fabs((float)cout_golden[r][c][o] - (float)cout[r][c][o]) > thres) {
+        // if (fabs((float)cout_golden[o][r][c] - (float)cout[o][r][c]) > thres) {
           err++;
         }
       }
