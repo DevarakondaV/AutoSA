4eec7df36839dfc94d22937daedadd67c9e3d6d1
devarakondav <devarakonda.vishnu5@gmail.com>
2021-11-29 22:44:34 -0800

    change load param names

--- autosa_tests/cnn/kernel.c
+++ autosa_tests/cnn/kernel.c
@@ -3,32 +3,32 @@
 
 int main(int argc, char **argv){
   // Allocate memory on heap to avoid stack overflow.
-  static data_t cin[I][R][C];
-  static data_t w[I][O][K][K];
+  static data_t input[I][R][C];
+  static data_t weight[I][O][K][K];
   static data_t bias[I];
-  static data_t cout[O][R][C];
+  static data_t output[O][R][C];
 
-  LoadData(cin, w, bias);
+  LoadData(input, weight, bias);
 
-  // data_t cin[R + K - 1][C + K - 1][I];
-  // data_t w[O][K][K][I];
-  // data_t cout[R][C][O];
-  // data_t cout_golden[R][C][O];
-  data_t cout_golden[O][R][C];
+  data_t cin[R + K - 1][C + K - 1][I];
+  data_t w[O][K][K][I];
+  data_t cout[R][C][O];
+  data_t cout_golden[R][C][O];
+  // data_t cout_golden[O][R][C];
 
-  // // data initialization
-  // for (int i = 0 ; i < I; i++)
-  //   for (int r = 0; r < R + K - 1; r++)
-  //     for (int c = 0; c < C + K - 1; c++) {
-  //       cin[r][c][i] = i;
-  //     }
+  // data initialization
+  for (int i = 0 ; i < I; i++)
+    for (int r = 0; r < R + K - 1; r++)
+      for (int c = 0; c < C + K - 1; c++) {
+        cin[r][c][i] = i;
+      }
 
-  // for (int o = 0; o < O; o++)
-  //   for (int i = 0; i < I; i++) 
-  //     for (int p = 0; p < K; p++)
-  //       for (int q = 0; q < K; q++) {
-  //         w[o][p][q][i] = o;
-  //       }
+  for (int o = 0; o < O; o++)
+    for (int i = 0; i < I; i++) 
+      for (int p = 0; p < K; p++)
+        for (int q = 0; q < K; q++) {
+          w[o][p][q][i] = o;
+        }
  
 #pragma scop
   for (int o = 0; o < O; o++)
@@ -38,8 +38,8 @@ int main(int argc, char **argv){
         for (int i = 0; i < I; i++)
           for (int p = 0; p < K; p++)
             for (int q = 0; q < K; q++) {
-              // cout[r][c][o] = cout[r][c][o] + cin[r + p][c + q][i] * w[o][p][q][i];
-              cout[o][r][c] = cout[o][r][c] + cin[i][r + p][c + q] * w[i][o][p][q];
+              cout[r][c][o] = cout[r][c][o] + cin[r + p][c + q][i] * w[o][p][q][i];
+              // cout[o][r][c] = cout[o][r][c] + cin[i][r + p][c + q] * w[i][o][p][q];
             }
       }
 #pragma endscop  
