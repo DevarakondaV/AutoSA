34fab2d46f4c4bb74bc41ba7c8695957789f7367
devarakondav <devarakonda.vishnu5@gmail.com>
2021-11-29 22:54:31 -0800

    dims to define

--- autosa_tests/cnn/kernel.h
+++ autosa_tests/cnn/kernel.h
@@ -20,11 +20,11 @@ typedef float data_t;
 // // #define RO 112 // kOutImSize
 // #define K 5 //kKernel
 
-const int kNum = 256;
-const int kKernel = 5;
-const int kImSize = 224;
-const int kInImSize = 228;
-const int kOutImSize = 112;
+#define kNum 256;
+#define kKernel 5;
+#define kImSize 224;
+#define kInImSize 228;
+#define kOutImSize 112;
 
 
 //#define O 6
